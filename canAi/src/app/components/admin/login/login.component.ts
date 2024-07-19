c4609d9a8b32854757e5e3c9d00be602aa09d4a6
lucho-netizen <codecors@hotmail.com>
2024-07-18 20:33:30 -0500

    Beta 1.0.0.1.9.1

--- canAi/src/app/components/admin/login/login.component.ts
+++ canAi/src/app/components/admin/login/login.component.ts
@@ -12,7 +12,6 @@ export class LoginAdminComponent implements OnInit {
   email: string = '';
   password: string = '';
   errorMessage: string = '';
-  // isLoggedIn: boolean = false;
   loginMessage: string = '';
 
   constructor(
@@ -54,25 +53,4 @@ export class LoginAdminComponent implements OnInit {
     }
   }
 
-  // onSubmit(): void {
-  //   if (this.email && this.password) {
-  //     this.AdminauthService.loginAdmin(this.email, this.password)
-  //       .subscribe(
-  //         (response: any) => {
-  //           if (response && response.token) {
-  //             this.AdminauthService.setTokenAdmin(response.token); // Guarda el token utilizando AuthService
-  //             this.router.navigate(['/dashboardadmin']); // Redirige al dashboard después del login exitoso
-  //           } else {
-  //             this.errorMessage = 'Credenciales inválidas'; // Mensaje de error si las credenciales son incorrectas
-  //           }
-  //         },
-  //         (error) => {
-  //           console.error('Error en el login:', error);
-  //           this.errorMessage = 'Error en el servidor. Por favor, inténtelo de nuevo más tarde.'; // Manejo de errores HTTP
-  //         }
-  //       );
-  //   } else {
-  //     this.errorMessage = 'Por favor, ingrese su correo electrónico y contraseña.';
-  //   }
-  // }
 }
